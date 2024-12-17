5a4a13ed875e4ec63ccb2deecd026d954b68fa2c
Nived <mnived054@gmail.com>
2024-12-17 18:42:02 +0530

    added some new topics and modifide some code in existing topics

--- maps/maps.go
+++ maps/maps.go
@@ -23,4 +23,18 @@ func exmp1() {
 	delete(myMap, "d")
 	fmt.Println(myMap)
 
+	newmap1 := make([]map[string]int, 4)
+
+	for i := range newmap1 {
+		newmap1[i] = make(map[string]int)
+	}
+
+	newmap1[0]["Key1"] = 10
+	newmap1[1]["Key2"] = 20
+	newmap1[2]["Key3"] = 30
+	newmap1[3]["Key4"] = 40
+
+	fmt.Println(newmap1)
+	fmt.Println(newmap1[0])
+	fmt.Println(newmap1[1])
 }
