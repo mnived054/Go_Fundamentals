821a1a0185d033af5b8bcc6f2b2928482323a7c9
Nived <mnived054@gmail.com>
2025-01-23 16:54:02 +0530

    ..

--- timestamp/timestamp.go
+++ timestamp/timestamp.go
@@ -6,6 +6,7 @@ import (
 )
 
 func main() {
+
 	currentTime := time.Now()
 
 	fmt.Println(currentTime)
@@ -15,4 +16,13 @@ func main() {
 
 	fmt.Println(time.Unix(currentTime.Unix(), 0))
 	fmt.Println(time.Unix(0, currentTime.UnixNano()))
+
+	specificDate := time.Date(2025, time.January, 23, 0, 0, 0, 0, time.UTC)
+	fmt.Println(specificDate)
+	fmt.Println(specificDate.Unix())
+	fmt.Println(specificDate.UnixMilli())
+	fmt.Println(specificDate.UnixNano())
+
+	fmt.Println(time.Unix(specificDate.Unix(), 0))
+	fmt.Println(time.Unix(0, specificDate.UnixNano()))
 }
